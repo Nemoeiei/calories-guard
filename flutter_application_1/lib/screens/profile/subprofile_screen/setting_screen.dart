e88fd3ba26c531d92874bd44875535c54fbd6e4d
Sirisak <sirisak.pie@student.mahidol.edu>
2026-03-19 03:26:47 +0700

    add: fix merge error This error make by mine. Because I want to merge this develop to main. But I didn't switch.

--- flutter_application_1/lib/screens/profile/subprofile_screen/setting_screen.dart
+++ flutter_application_1/lib/screens/profile/subprofile_screen/setting_screen.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFE8EFCF),
+      appBar: AppBar(
+        title: const Text('ตั้งค่า', style: TextStyle(color: Colors.black)),
+        backgroundColor: const Color(0xFF4C6414),
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: const Center(
+        child: Text(
+          'หน้าตั้งค่ากำลังอยู่ในช่วงพัฒนา',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
